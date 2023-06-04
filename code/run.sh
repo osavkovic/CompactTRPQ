476d67219f408b732cf72ec8d925b9c3cbde25db
Anton Dignös <dignoes@inf.unibz.it>
2023-06-04 16:40:48 +0200

    ad

---
+++ code/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# this code assumes an environment variable PSQL that is the the psql command with the connection parameters
+
+for gran in 1 2 # add desired domain scaling factor k here
+do
+
+  echo "----------------------------------------------------------------------------" >> out.txt
+  echo "-- Granularity $gran times the raw granularity " >> out.txt
+  echo "----------------------------------------------------------------------------" >> out.txt
+
+  cat template_data_setup.sql > data_setup.sql
+  sed -i "s/???/${gran}/g" data_setup.sql
+  $PSQL -f data_setup.sql >> out.txt
+
+  for i in 1 2 # add desired duration x here
+  do
+    cat template_contact_-X_0.sql > contact_-${i}_0.sql
+    sed -i "s/???/${i}/g" contact_-${i}_0.sql
+    sed -i "s/?!?/${gran}/g" contact_-${i}_0.sql
+
+    echo "----------------------------------------------------------------------------" >> out.txt
+    echo "-- (positive = true)/T[-${i}, 0]/F/meets/ " >> out.txt
+    echo "----------------------------------------------------------------------------" >> out.txt
+    $PSQL -f contact_-${i}_0.sql >> out.txt
+  done
+done
